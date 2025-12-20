24d57c452d70448a0999727e36019ee28c910951
bala-amypo <bala@amypo.in>
2025-12-20 10:25:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Department.java
@@ -0,0 +1,70 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+
+    private String requiredSkills;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Department() {
+    }
+
+    public Department(String name, String description, String requiredSkills) {
+        this.name = name;
+        this.description = description;
+        this.requiredSkills = requiredSkills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getDescription() {
+        return description;
+    }
+ 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+ 
+    public String getRequiredSkills() {
+        return requiredSkills;
+    }
+ 
+    public void setRequiredSkills(String requiredSkills) {
+        this.requiredSkills = requiredSkills;
+    }
+ 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+ 
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
