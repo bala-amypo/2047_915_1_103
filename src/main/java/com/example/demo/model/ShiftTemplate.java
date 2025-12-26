cc89b248e501aad3ab16e2ac8531d4cda8ed50c1
bala-amypo <bala@amypo.in>
2025-12-26 16:25:35 +0000

    User commit

--- src/main/java/com/example/demo/model/ShiftTemplate.java
+++ src/main/java/com/example/demo/model/ShiftTemplate.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+
 import java.time.LocalTime;
 
 @Entity
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+
 
 @Entity
 @Table(name = "users")
