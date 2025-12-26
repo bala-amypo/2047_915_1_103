f58cfe0224e33d8c07638c4d4d2a817ba111616e
bala-amypo <bala@amypo.in>
2025-12-26 14:39:24 +0000

    User commit

--- src/main/java/com/example/demo/dto/ShiftTemplateDto.java
+++ src/main/java/com/example/demo/dto/ShiftTemplateDto.java
@@ -1,5 +1,64 @@
-package com.example.demo.controller.dto;
+package com.example.demo.dto;
+
+import java.time.LocalTime;
 
 public class ShiftTemplateDto {
-    
+
+    private String templateName;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String requiredSkills;
+    private Long departmentId;
+
+    public ShiftTemplateDto() {}
+
+    public ShiftTemplateDto(String templateName, LocalTime startTime,
+                            LocalTime endTime, String requiredSkills,
+                            Long departmentId) {
+        this.templateName = templateName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.requiredSkills = requiredSkills;
+        this.departmentId = departmentId;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getRequiredSkills() {
+        return requiredSkills;
+    }
+
+    public void setRequiredSkills(String requiredSkills) {
+        this.requiredSkills = requiredSkills;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
 }
